3fb038d1a5bf58bf948d02b9541716389af522ac
Marcus <anymre@outlook.com>
2018-12-19 10:50:11 +0800

    1.0
    config discovery

--- Client/src/main/java/com/connext/springcloud/EurekaClientApplication.java
+++ Client/src/main/java/com/connext/springcloud/EurekaClientApplication.java
@@ -1,11 +1,13 @@
 package com.connext.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,20 +18,32 @@ import java.util.List;
 @SpringBootApplication
 public class EurekaClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaClientApplication.class, args);
+  }
 }
 
 @RestController
 class ServiceInstanceRestController {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+  @Autowired private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
+  @RequestMapping("/service-instances/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(
+      @PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
+  }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+  @Value("${message:Hello default}")
+  private String message;
+
+  @RequestMapping("/message")
+  String getMessage() {
+    return this.message;
+  }
 }
